3a0ddaccd1d9b9167267eced41c380afdd0e0f6c
f0laf0lu <ofolafolu@gmail.com>
2026-04-15 21:14:05 +0100

    allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!6@qle_6)z!k-o@r$b6xy+hshl_vjrr*xh7plgy-=pv7q18%*c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
